b0382ebf4bff3e7781c7c5013579d0a93f2fc324
Sameer <mesameer191@gmail.com>
2020-03-08 12:26:42 +0530

    Big o Notation with a simple example.

--- 1.intro/Big_O.js
+++ 1.intro/Big_O.js
@@ -36,9 +36,38 @@ console.log(t4-t3)
 
 //OUTPUT: ~1ms
 
-// above measuring technique is good but if you run these exact code on exact computer.
-// the results vary. that shows these timing technique.
-
+// above measuring technique is good but if you run these exact code on exact computer,
+// the results vary. that shows these timing technique. are not accurate enough.
 //so we came up with counting Operations rather than timing.
 
+// on second example we can see the number of operations rather values are not bound to any loop or any other variable
+//on the other hand first exampple n is bound to a loop that will execute n times. so 
 
+//first example will have Oder of n  i.e O(n) and secound example have O(1) time.
+
+
+/*****************************
+ * Big O for Objects.
+ * ------------------------------------------
+ * all bisic operation takes const time O(1) such as insertion, removal, accessing.
+ * searching will take O(N). i.e searching with values. or to check if some values is there in the object.
+ * 
+ * Methods of Objects.
+ * keys() - O(N)
+ * entries() - O(N)
+ * values() - O(N)
+ * 
+ * hasOwnProperty() - O(1)
+ */
+
+ /*********************************
+  * Big O for Arrys.
+  * all items are ordered
+  * access the item.(random access is possible) - O(1)
+  * insertion and deletion is compled - O(N) (when inserted at the begining.)
+  * searching O(N)
+  * 
+  * sorting O(NlogN)
+  */
+
+  
